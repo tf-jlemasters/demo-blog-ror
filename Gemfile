36256a05748ffe93e9299d0c3f96deb7294f45a1
tf-jlemasters <jlemasters@tracefirst.com>
2022-08-09 09:48:05 -0600

    updated gems due to security risks

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ gem "bootsnap", require: false
 
 gem 'bulma-rails', '~> 0.9.3'
 gem 'simple_form', '~> 5.1'
+gem 'devise', github: 'heartcombo/devise', branch: 'main'
 
 
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_06_210920) do
-  create_table "comments", force: :cascade do |t|
-    t.string "name"
-    t.text "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "post_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
